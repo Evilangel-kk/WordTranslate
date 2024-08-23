b423520bd449d58e2368a7f5c30976384d376c9a
Evilangel <2472047312@qq.com>
2024-08-23 20:00:23 +0800

    v1.0.1:回车直接进行搜素

--- app/src/main/java/com/example/translate/fragment/SearchPageFragment.kt
+++ app/src/main/java/com/example/translate/fragment/SearchPageFragment.kt
@@ -1,24 +1,22 @@
 package com.example.translate.fragment
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.translate.GetTranslate
 import com.example.translate.Letter
 import com.example.translate.LetterMeaningsListFragment
 import com.example.translate.MyDataBaseHelper
 import com.example.translate.databinding.FragmentSearchPageBinding
-import com.example.translate.publicFunction
-import com.example.translate.publicFunction.Companion.getCurrentDate
 import com.example.translate.setupEditText
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+
 
 /**
  * A simple [Fragment] subclass.
@@ -29,6 +27,7 @@ class SearchPageFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchPageBinding
     private lateinit var dbHelper: MyDataBaseHelper
+    private lateinit var fragment: LetterMeaningsListFragment
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -43,36 +42,26 @@ class SearchPageFragment : Fragment() {
         dbHelper=MyDataBaseHelper(requireContext(), "letterdata.db", 1)
 
         // 创建 LetterMeaningsListFragment 实例
-        val fragment = LetterMeaningsListFragment()
+        fragment = LetterMeaningsListFragment()
         // 动态添加 Fragment
         childFragmentManager.beginTransaction()
             .replace(binding.letterMeaningsListFrag.id, fragment)
             .commit()
         binding.letterMeaningsListFrag.visibility=View.INVISIBLE
-        binding.mySearchBtn.setOnClickListener {
-            if(binding.myEditText.text.toString().trim().isNotEmpty()){
-                // 获取输入的文本
-                Letter.changeWord(binding.myEditText.text.toString())
-                Log.d("Letter.changeWord", "KeyWord:"+binding.myEditText.text.toString())
-                Log.d("getTranslate", "等待更新Fragment")
-                // 获取翻译并更新 Fragment
-                getTranslate { result ->
-                    if (result == 1) {
-                        fragment.setMeanings(1)
-                        Log.d("getTranslate", "更新到Fragment")
-    //                    dbHelper.insertOrUpdateRecord(getCurrentDate(), 1)
-                        requireActivity().runOnUiThread{
-                            binding.theWord.text = Letter.word
-                            binding.letterMeaningsListFrag.visibility=View.VISIBLE
-                        }
-                    }else{
-                        requireActivity().runOnUiThread{
-                            binding.theWord.text = "无结果"
-                            binding.letterMeaningsListFrag.visibility=View.INVISIBLE
-                        }
-                    }
-                }
+
+        binding.myEditText.setOnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // 处理回车事件
+                Log.d("actionId","EditorInfo.IME_ACTION_DONE")
+                sendRequest()
+                true
+            } else {
+                false
             }
+        }
+
+        binding.mySearchBtn.setOnClickListener {
+            sendRequest()
         }
     }
 
@@ -83,6 +72,32 @@ class SearchPageFragment : Fragment() {
                 callback(1)
             }else{
                 callback(0)
+            }
+        }
+    }
+
+    fun sendRequest(){
+        if(binding.myEditText.text.toString().trim().isNotEmpty()){
+            // 获取输入的文本
+            Letter.changeWord(binding.myEditText.text.toString())
+            Log.d("Letter.changeWord", "KeyWord:"+binding.myEditText.text.toString())
+            Log.d("getTranslate", "等待更新Fragment")
+            // 获取翻译并更新 Fragment
+            getTranslate { result ->
+                if (result == 1) {
+                    fragment.setMeanings(1)
+                    Log.d("getTranslate", "更新到Fragment")
+                    //                    dbHelper.insertOrUpdateRecord(getCurrentDate(), 1)
+                    requireActivity().runOnUiThread{
+                        binding.theWord.text = Letter.word
+                        binding.letterMeaningsListFrag.visibility=View.VISIBLE
+                    }
+                }else{
+                    requireActivity().runOnUiThread{
+                        binding.theWord.text = "无结果"
+                        binding.letterMeaningsListFrag.visibility=View.INVISIBLE
+                    }
+                }
             }
         }
     }
